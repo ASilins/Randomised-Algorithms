073446c46b27eeaffc239909220868ff16598cf2
Arturs Silins <arturs.silins45@gmail.com>
2025-03-14 22:19:03 +0100

    Run for handin

--- utils/NumberUtils.cpp
+++ utils/NumberUtils.cpp
@@ -1,5 +1,8 @@
 #include "NumberUtils.h"
 
+#include <iostream>
+#include <ostream>
+
 uint32_t NumberUtils::generate_random_uint(const uint32_t m)  {
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -19,6 +22,11 @@ uint64_t NumberUtils::generate_random_uint(const uint64_t m) {
 std::vector<uint64_t> NumberUtils::generate_shuffled_list(uint32_t size) {
     std::vector<uint64_t> list(size);
     for (uint64_t i = 1; i <= size; ++i) {
+        auto result = i*i;
+        if (result > (1ULL >> 61) - 1) {
+            std::cout << "Number is too large: " << result << std::endl;
+            continue;
+        }
         list[i-1] = i*i;
     }
     std::random_device rd;
